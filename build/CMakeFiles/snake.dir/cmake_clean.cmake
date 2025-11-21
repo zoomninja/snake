11cd544d29f22f643cde1a61248a6553b965daed
Joshua Kamil <joshuaykamil@gmail.com>
2025-11-20 20:35:13 -0800

    whatever

---
+++ build/CMakeFiles/snake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snake.dir/main.c.o"
+  "CMakeFiles/snake.dir/main.c.o.d"
+  "snake"
+  "snake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/snake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
